ec1d3c74c4d5064ce349f149d43812480d33a8ba
amine-malyana <aminemalyana12@gmail.com>
2023-02-23 21:52:55 +0100

    category crud

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
@@ -11,3 +12,8 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('refresh', 'refresh');
     Route::get('me', 'me');
 });
+
+Route::get('category', [CategoryController::class, 'index']);
+Route::post('category', [CategoryController::class, 'store']);
+Route::get('category/{id}/edit', [CategoryController::class, 'edit']);
+Route::put('category/{id}/edit', [CategoryController::class, 'update']);

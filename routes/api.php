eb510971aaa4886913607d20b08d5a5546b3d079
RedOuaneAO <redouaneao97@gmail.com>
2023-02-23 21:01:21 +0100

    add index function to display all the articles

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ArticleController;
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
@@ -11,3 +12,10 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('refresh', 'refresh');
     Route::get('me', 'me');
 });
+
+
+Route::get('/index' , [ArticleController::class , "index"]);
+Route::post('/addArticle' , [ArticleController::class , "addArticle"]);
+Route::get('/show/{id}' , [ArticleController::class , "showArticle"]);
+Route::put('/update/{id}' , [ArticleController::class , "updateArticle"]);
+Route::delete('/delete/{id}' , [ArticleController::class , "deleteArticle"]);
